380c37433aa2e914823ad996d96bbbe29851e357
Camelia Ariana <67178761+camelia02@users.noreply.github.com>
2022-06-26 19:22:00 +0800

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,13 @@
+package DB_Testing;
+
+public class Employee {
+    public String Fname;
+    public int ID;
+
+    public Employee(int ID, String Fname) {
+        this.ID = ID;
+        this.Fname = Fname;
+    }
+
+
+}
---
+++ Select.java
@@ -0,0 +1,11 @@
+package DB_Testing;
+import java.sql.*;
+import java.util.*;
+
+
+public class Select extends SQLConnection{
+    public static void main(String[] args){
+
+
+    }
+}
