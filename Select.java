9a1a7008b9a63639676f9557cb30cc2686a21dd6
Camelia Ariana <67178761+camelia02@users.noreply.github.com>
2022-06-28 21:11:35 +0800

    Update Select.java

--- Select.java
+++ Select.java
@@ -1,11 +1,38 @@
 package DB_Testing;
-import java.sql.*;
-import java.util.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 
-public class Select extends SQLConnection{
-    public static void main(String[] args){
+public class Connector {
 
-
+    public static Connection Connect() throws SQLException{
+        return DriverManager.getConnection("jdbc:sqlserver://localhost:1433;database=test_Database;user=Camelia;password=test; encrypt=true; trustServerCertificate=true");
     }
+
+    public static String Login(){
+        String usertype = JOptionPane.showInputDialog("Please enter your usertype: ");
+        String username = JOptionPane.showInputDialog("Enter Username");
+        String password = JOptionPane.showInputDialog("Enter Password");
+
+        if(usertype.equals("admin")){
+            if(username.equals("admin") && password.equals("admin")){
+            return "admin";
+                
+            }
+        }
+        else if(usertype.equals("user")){
+            if(username.equals("user") && password.equals("user")){
+                return "user";
+            }
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "Login Failed");
+        }
+        return "";
+    }
+
+  
 }
