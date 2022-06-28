f0e69859d6edfbe02d087cc8ae7476de2030fddd
Camelia Ariana <67178761+camelia02@users.noreply.github.com>
2022-06-28 21:10:33 +0800

    Update and rename SQLConnection.java to DatabaseManager.java

---
+++ DatabaseManager.java
@@ -0,0 +1,13 @@
+package DB_Testing;
+import java.sql.*;
+import java.util.*;
+
+public class DatabaseManager{
+  public static void main(String[] args) {
+
+  DB_Manager manager = new DB_Manager();
+  manager.Menu();
+  
+}
+}
+
